db13f2489ee979fa5d083ffdb3184643996fb3a4
jaeyoungjang2 <lion85488@gmail.com>
2021-05-24 20:43:13 +0900

    add static html

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,14 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    publc String hello(Model model){
-        model.addAllAttributes("data","hello!!")
-
+    public String hello(Model model){
+        model.addAttribute("data","hello!!!");
+        return "hello";
     }
+
 }
