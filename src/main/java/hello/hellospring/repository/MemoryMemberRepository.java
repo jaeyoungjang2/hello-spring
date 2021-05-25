cdd83017cff95bce2d6951ec0dc754d8b7f0c262
jaeyoungjang2 <lion85488@gmail.com>
2021-05-25 20:30:26 +0900

    add member repository, memeber domain

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null 이 반환 될 가능성이 있으면 Optional.ofNullable을 사용해서 반환해주면 client에서 뭘 할 수 있다. 뒤에서 배울 예정
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    // lambda 사용, 같은 것이 있으면 뽑아내고 (filter) , 없으면 null 반환
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public Optional<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
